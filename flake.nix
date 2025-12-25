51ccf4237b9f6a073528baf8ff95a1944248416e
Jiho Song <me@citrus.name>
2025-12-25 17:16:12 +0000

    fix(darwin): use home-manager release-24.11 instead of master
    home-manager master has a bug where lib.genAttrs' is not properly
    exposed. Using release-24.11 which is stable and compatible with
    nixos-unstable.

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     };
 
     home-manager-unstable = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
 
