392364e5126951a6da7720ff7d8c08425a43542b
Jiho Song <jihojiho2003@gmail.com>
2026-01-01 03:22:52 +0900

    chore: remove devShells, add backup extension
    - Remove devShells from flake.nix
    - Add home-manager.backupFileExtension for safe rebuilds

--- lib.nix
+++ lib.nix
@@ -1,35 +1,55 @@
 { inputs, overlays }:
 
 {
-  mkNixosConfig = { nixpkgs, home-manager }: { system, hostName, username, modules ? [] }:
+  mkNixosConfig =
+    { nixpkgs, home-manager }:
+    {
+      system,
+      hostName,
+      username,
+      modules ? [ ],
+    }:
     nixpkgs.lib.nixosSystem {
       inherit system;
       specialArgs = { inherit inputs username; };
       modules = [
         { nixpkgs.overlays = builtins.attrValues overlays; }
         ./hosts/${hostName}
-        home-manager.nixosModules.home-manager {
+        home-manager.nixosModules.home-manager
+        {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "bak";
           home-manager.users.${username} = import ./home/${username};
           home-manager.extraSpecialArgs = { inherit inputs username; };
         }
-      ] ++ modules;
+      ]
+      ++ modules;
     };
 
-  mkDarwinConfig = { darwin, home-manager }: { system, hostName, username, modules ? [] }:
+  mkDarwinConfig =
+    { darwin, home-manager }:
+    {
+      system,
+      hostName,
+      username,
+      modules ? [ ],
+    }:
     darwin.lib.darwinSystem {
       inherit system;
       specialArgs = { inherit inputs username; };
       modules = [
         { nixpkgs.overlays = builtins.attrValues overlays; }
         ./hosts/${hostName}
-        home-manager.darwinModules.home-manager {
+        home-manager.darwinModules.home-manager
+        {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "bak";
           home-manager.users.${username} = import ./home/${username};
           home-manager.extraSpecialArgs = { inherit inputs username; };
         }
-      ] ++ modules;
+      ]
+      ++ modules;
     };
 }
