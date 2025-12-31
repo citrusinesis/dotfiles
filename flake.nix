62507a11b78ab609d0bb76668c260bbc140255ae
Jiho Song <jihojiho2003@gmail.com>
2026-01-01 03:22:52 +0900

    chore: remove devShells, add backup extension
    - Remove devShells from flake.nix
    - Add home-manager.backupFileExtension for safe rebuilds

--- flake.nix
+++ flake.nix
@@ -16,43 +16,50 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, darwin, home-manager, ... }@inputs:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      darwin,
+      home-manager,
+      ...
+    }@inputs:
     let
-      supportedSystems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
       personal = import ./personal.nix;
       overlays = import ./overlays { inherit inputs; };
       lib = import ./lib.nix { inherit inputs overlays; };
-    in {
-      nixosConfigurations.blender = lib.mkNixosConfig {
-        nixpkgs = nixpkgs;
-        home-manager = home-manager;
-      } {
-        system = "x86_64-linux";
-        hostName = "blender";
-        username = personal.user.username;
-      };
-
-      darwinConfigurations.squeezer = lib.mkDarwinConfig {
-        darwin = darwin;
-        home-manager = home-manager;
-      } {
-        system = "aarch64-darwin";
-        hostName = "squeezer";
-        username = personal.user.username;
-      };
-
-      devShells = forAllSystems (system:
-        let pkgs = nixpkgs.legacyPackages.${system};
-        in {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              nixpkgs-fmt
-              nil
-            ];
+    in
+    {
+      nixosConfigurations.blender =
+        lib.mkNixosConfig
+          {
+            nixpkgs = nixpkgs;
+            home-manager = home-manager;
+          }
+          {
+            system = "x86_64-linux";
+            hostName = "blender";
+            username = personal.user.username;
           };
-        }
-      );
+
+      darwinConfigurations.squeezer =
+        lib.mkDarwinConfig
+          {
+            darwin = darwin;
+            home-manager = home-manager;
+          }
+          {
+            system = "aarch64-darwin";
+            hostName = "squeezer";
+            username = personal.user.username;
+          };
     };
 }
