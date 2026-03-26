c33c707cfc6e7662f68ec253235000c9287935c0
Jiho Song <jihojiho2003@gmail.com>
2026-03-26 10:56:30 +0900

    fix: resolve build error (`cgo` not enabled)  while building direnv

--- modules/home/dev/direnv.nix
+++ modules/home/dev/direnv.nix
@@ -7,6 +7,11 @@
 
   programs.direnv = {
     enable = true;
+    package = pkgs.direnv.overrideAttrs (old: {
+      env = (old.env or { }) // {
+        CGO_ENABLED = "1";
+      };
+    });
     nix-direnv.enable = true;
 
     config = {
