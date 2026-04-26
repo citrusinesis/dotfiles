1e36d09c595c7e4fb52f93715c90f380ff4d1a2a
Jiho Song <jihojiho2003@gmail.com>
2026-04-26 17:43:42 +0900

    feat(kubernetes): add helm, flux, and cluster tooling

--- modules/home/dev/kubernetes.nix
+++ modules/home/dev/kubernetes.nix
@@ -3,11 +3,29 @@
 {
   home.packages = with pkgs; [
     kubectl
+    kubectx
+
+    kubernetes-helm
+    helmfile
+
+    kustomize
+    k9s
+    stern
+
+    fluxcd
   ];
 
   programs.zsh.initContent = lib.mkOrder 560 ''
     if command -v kubectl &> /dev/null; then
       source <(kubectl completion zsh)
+    fi
+
+    if command -v helm &> /dev/null; then
+      source <(helm completion zsh)
+    fi
+
+    if command -v flux &> /dev/null; then
+      source <(flux completion zsh)
     fi
   '';
 }
