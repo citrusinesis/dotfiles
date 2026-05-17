70b7e228f722621b522cf7bdd069570edc7112c1
Jiho Song <me@citrus.name>
2026-05-17 17:30:50 +0900

    feat(nix): update settings and add package configuration

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -1,9 +1,13 @@
-# Nix daemon and package manager settings
-{ ... }:
+{ flake, pkgs, ... }:
 
+let
+  personal = import (flake.inputs.self + /personal.nix);
+in
 {
   nix = {
     optimise.automatic = true;
+    package = pkgs.lixPackageSets.stable.lix;
+
     settings = {
       experimental-features = [
         "nix-command"
@@ -16,16 +20,17 @@
       substituters = [
         "https://cache.nixos.org/"
         "https://nix-community.cachix.org"
+        "https://cache.lix.systems"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
       ];
 
       trusted-users = [
         "root"
-        "@wheel"
-        "@admin"
+        (if pkgs.stdenv.isDarwin then personal.user.username else "@wheel")
       ];
     };
 
@@ -35,5 +40,11 @@
     };
   };
 
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs = {
+    config = {
+      allowBroken = true;
+      allowUnsupportedSystem = true;
+      allowUnfree = true;
+    };
+  };
 }
