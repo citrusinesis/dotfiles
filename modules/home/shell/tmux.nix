637d57f677daff25068a92541445e173e9909cbd
Jiho Song <me@citrus.name>
2026-06-05 11:15:16 +0900

    feat(tmux): fix that not loading plugin

--- modules/home/shell/tmux.nix
+++ modules/home/shell/tmux.nix
@@ -5,7 +5,6 @@
     enable = true;
     mouse = true;
     terminal = "tmux-256color";
-    baseIndex = 1;
     escapeTime = 0;
     historyLimit = 10000;
 
@@ -17,8 +16,6 @@
           set -g @catppuccin_window_status_style "rounded"
         '';
       }
-      cpu
-      battery
       {
         plugin = resurrect;
         extraConfig = ''
@@ -36,6 +33,16 @@
     ];
 
     extraConfig = ''
+      set -as terminal-features ',xterm-256color:RGB'
+      set -as terminal-features ',tmux-256color:RGB'
+
+      set -g extended-keys on
+      set -g extended-keys-format csi-u
+
+      set -g renumber-windows on
+      set -g set-clipboard on
+      set -g focus-events on
+
       bind | split-window -h
       bind _ split-window -v
       unbind '"'
@@ -46,14 +53,22 @@
       bind -n M-Up select-pane -U
       bind -n M-Down select-pane -D
 
+      bind r source-file ~/.config/tmux/tmux.conf \; display-message "tmux.conf reloaded"
+
       set -g status-right-length 100
       set -g status-left-length 100
       set -g status-left ""
+
       set -g status-right "#{E:@catppuccin_status_application}"
       set -agF status-right "#{E:@catppuccin_status_cpu}"
+      set -agF status-right "#{E:@catppuccin_status_ram}"
       set -ag status-right "#{E:@catppuccin_status_session}"
       set -ag status-right "#{E:@catppuccin_status_uptime}"
       set -agF status-right "#{E:@catppuccin_status_battery}"
+
+      set -g status-interval 5
+      run-shell ${pkgs.tmuxPlugins.cpu}/share/tmux-plugins/cpu/cpu.tmux
+      run-shell ${pkgs.tmuxPlugins.battery}/share/tmux-plugins/battery/battery.tmux
     '';
   };
 }
